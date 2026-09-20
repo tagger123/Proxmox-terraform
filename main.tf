aa353ed534ab3f90517f962455d757f4ea8c651a
Michał Sawczuk <49233475+tagger123@users.noreply.github.com>
2026-09-20 18:18:47 +0200

    update

--- main.tf
+++ main.tf
@@ -1,28 +1,12 @@
 resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
-#   vm_id     = 100         # Sztywne ID maszyny (musi być wolne w Proxmoxie)
-#   node_name = "pve"       # Nazwa Twojego węzła Proxmox
-#   name      = "ubuntu-vm" # Nazwa hosta nowej maszyny
-
-  agent {
-    enabled = true
-  }
-
+  node_name = "pve-01" # Nazwa Twojego noda w Proxmoxie
+  name      = "terraform-test-vm"
   cpu {
     cores = 2
-    type  = "x86-64-v2-AES"
+    cores = 4
   }
-
   memory {
-    dedicated = 2048 # 2 GB RAM
+    dedicated = 2048
   }
-
-  disk {
-    datastore_id = "local-lvm" # Nazwa Twojego storage w Proxmoxie
-    file_id      = ""         # Tutaj możesz wskazać obraz ISO lub zostawić puste
-    size         = 20         # 20 GB dysku
-  }
-
-  network_device {
-    bridge = "vmbr0" # Twoja sieć / bridge w Proxmoxie
-  }
+  # Przykładowa konfiguracja dysku, sieci itp.
 }
--- provider.tf
+++ provider.tf
@@ -8,19 +8,8 @@ terraform {
   }
 }
 
-#variable "proxmox_api_url" {
-#  type        = string
-#  description = "Adres URL API Proxmoxa"
-#}
-
-#variable "proxmox_api_token" {
-#  type        = string
-#  sensitive   = true
-#  description = "Token API Proxmoxa w formacie user@realm!token=uuid"
-#}
-
 provider "proxmox" {
   endpoint  = var.proxmox_api_url
   api_token = var.proxmox_api_token
-  insecure  = true # Pomija błędy samo-podpisanych certyfikatów SSL
+  insecure  = true
 }
