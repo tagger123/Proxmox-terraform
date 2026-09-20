17463e083cc7d6ccb8dc559ad79c7fc436faddb3
Michał Sawczuk <49233475+tagger123@users.noreply.github.com>
2026-09-20 18:19:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
   node_name = "pve-01" # Nazwa Twojego noda w Proxmoxie
   name      = "terraform-test-vm"
   cpu {
-    cores = 2
     cores = 4
   }
   memory {
